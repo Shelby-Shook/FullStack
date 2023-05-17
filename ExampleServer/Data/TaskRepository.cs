f333d6ce8879b9e8faf0d24a94c72db05cf82e98
Shelby Shook <shelby.shook1217@gmail.com>
2023-05-17 11:24:49 -0400

    Added TaskRepository with Create, Read, and Delete

---
+++ ExampleServer/Data/TaskRepository.cs
@@ -0,0 +1,60 @@
+//Task Repository is responsible for storing and manipulating
+// our collection of data, in this case TaskModels
+
+namespace ExampleServer.Data;
+
+public class TaskRepository
+{
+    // Data storage (All of our tasks)
+    private readonly List<TaskModel> _taskList = new List<TaskModel>();
+
+    //Create method
+    //Not creating an instance, we're creating an entry in the list
+
+    public void AddTask(TaskModel task)
+    {
+        _taskList.Add(task);
+        //_taskList.Contains(task);
+    }
+
+    //Read method
+    public List<TaskModel> GetTasks()
+    {
+        return new List<TaskModel>(_taskList);
+    }
+
+    public List<TaskModel> GetTasksByStatus(bool isComplete)
+    {
+        //Start a new list
+        List<TaskModel> tasks = new List<TaskModel>();
+
+        // Iterate through all tasks and check the status
+        foreach (TaskModel task in _taskList)
+        {
+            //Add a task to the new list if its status matches the parameter
+            if (task.IsComplete == isComplete)
+            {
+                tasks.Add(task);
+            }
+        }
+        //Return the new list
+        return tasks; 
+    }
+
+    //Delete method
+    public bool DeleteTaskById(int id)
+    {
+        //Loop through each task
+        foreach(var task in _taskList)
+        {
+            // Check the task Id against our parameter
+            if (task.Id == id)
+            {
+                // If we find it remove the task and return true/false
+                return _taskList.Remove(task);
+            }
+        }
+        //Return false if we don't find the Id in the loop
+        return false;
+    }
+}
--- ExampleServer/Program.cs
+++ ExampleServer/Program.cs
@@ -12,3 +12,30 @@ task1.WriteTotalTasks();
 task2.WriteTotalTasks();
 
 Console.WriteLine(task2.Id);
+
+// Implicit Types
+// var assumes the type from the righthand side of the expression
+var task3 = new TaskModel("Task 3", "The third task");
+
+//Target-typed new
+// Implicit new will assume the type from the lefthand side
+TaskModel task4 = new("Task 4", "The fourth task");
+
+task1.IsComplete = true;
+task4.IsComplete = true;
+
+TaskRepository repo = new();
+repo.AddTask(task1);
+repo.AddTask(task2);
+repo.AddTask(task3);
+repo.AddTask(task4);
+
+
+repo.DeleteTaskById(3);
+
+// tasks gets its type from the GetTasks() return type
+var tasks = repo.GetTasksByStatus(true);
+foreach (var task in tasks)
+{
+    Console.WriteLine(task.Description);
+}
